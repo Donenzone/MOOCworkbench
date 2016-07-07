2b63980fc7f6d659f25fc698ace4aad0fad12f3b
Jochem <jochem@degoede.email>
2016-07-07 10:44:27 +0200

    Added Worker and WorkerManager apps, added some Git repo code

--- GitManager/Modules/Git.py
+++ GitManager/Modules/Git.py
@@ -2,7 +2,20 @@ from git import Repo
 from MOOCworkbench.settings import BASE_DIR
 
 
-def create_new_repository(repository_name, type):
-    bare_repo = Repo.init(BASE_DIR + '/' + repository_name, bare=True)
+def create_new_repository(repository_name, user, type):
+    path = BASE_DIR + '/' + user + '/bare_repos/' + repository_name
+    bare_repo = Repo.init(path, bare=True)
+    clone_bare_repo(repository_name, bare_repo, user)
 
 
+def clone_bare_repo(repository_name, repo, user):
+    cloned_repo = repo.clone(BASE_DIR + '/' + user + '/' + repo.working_dir)
+
+    # write base file
+    file_name = 'main.py'
+    open(cloned_repo.working_dir + '/' + file_name, 'a').close()
+
+    # commit and push base file
+    cloned_repo.index.add([file_name])
+    cloned_repo.index.commit("Initial commit by MOOC workbench")
+    cloned_repo.remote().push()
---
+++ WorkerManager/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class WorkermanagerConfig(AppConfig):
+    name = 'WorkerManager'

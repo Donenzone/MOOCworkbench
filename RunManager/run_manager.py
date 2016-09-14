28f35a25ab5f121706f651c0a239be3c4cfc1d3e
Jochem <jochem@degoede.email>
2016-09-14 10:40:09 +0200

    Docker instance creation

--- RunManager/run_manager.py
+++ RunManager/run_manager.py
@@ -4,27 +4,33 @@ from shutil import copyfile
 import fileinput
 from docker import Client
 from io import BytesIO
+import os
+from subprocess import Popen, PIPE
 
 dockerfile_str = '''
 FROM python:3
-COPY home/jochem/Development/MOOCworkbench/RunManager/gitrepositories/{CURRENT_REPO} /repo
+COPY gitrepositories/{CURRENT_REPO} /repo
 WORKDIR /repo
 RUN pip install -r requirements.txt
 '''
 
 @task
-def start_code_run(path_to_repo, repo_name):
-    print("About to start execution of script")
+def setup_docker_image(path_to_repo, repo_name):
     print("Setting up Docker environment")
-    dockerfile = BytesIO(create_docker_file(repo_name).encode('utf-8'))
-    cli = Client(base_url='unix://var/run/docker.sock')
-    response = [line for line in cli.build(fileobj=dockerfile, rm=True,  tag='moocworkbench/volume')]
-    for line in response:
-        print(line)
-    print(path_to_repo)
+    create_docker_file(repo_name)
+    p = Popen(['docker', 'build', '-t', 'moocworkbench', '.', '--tag', 'moocworkbench/mooc'],
+              stdout=PIPE)
+    print(p.stdout.read().decode('utf-8'))
+
+
+def start_code_run():
+    print("Starting code execution")
+    p_run = Popen(['docker', 'run', 'moocworkbench/mooc', 'python', 'main.py'],
+              stdout=PIPE)
+    print(p_run.stdout.read().decode('utf-8'))
 
 
 def create_docker_file(repo_name):
-    return dockerfile_str.replace('{CURRENT_REPO}', repo_name)
-
-start_code_run('/gitrepositories/my-first-repo', 'my-first-repo')
+    dockerfile = dockerfile_str.replace('{CURRENT_REPO}', repo_name)
+    with open('Dockerfile', 'w+') as f:
+        f.write(dockerfile)

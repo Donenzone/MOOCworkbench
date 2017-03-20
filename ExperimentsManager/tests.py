21149eb8d128c405d0b69991d1855e99ced951c7
Jochem de Goede <jochem@degoede.email>
2017-03-20 17:27:20 +0100

    Test fixed: WorkbenchUser is auto created by signal, so creating it separately is not required

--- ExperimentsManager/tests.py
+++ ExperimentsManager/tests.py
@@ -8,7 +8,7 @@ from django.test import Client
 class ExperimentTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user('test', 'test@test.nl', 'test')
-        self.workbench_user = WorkbenchUser.objects.create(netid='jlmdegoede', user=self.user)
+        self.workbench_user = WorkbenchUser.objects.get(user=self.user)
         self.experiment = Experiment.objects.create(title='Experiment', description='test', version='1.0', owner=self.workbench_user)
 
     def test_index_not_signed_in(self):

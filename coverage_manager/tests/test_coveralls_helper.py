b567f5521fc9f2f53c5a6a46d81f47c044e2f159
Jochem de Goede <jochem@degoede.email>
2017-04-25 16:42:52 +0200

    CoverallsHelper tests

---
+++ coverage_manager/tests/test_coveralls_helper.py
@@ -0,0 +1,37 @@
+import unittest
+
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+
+from coverage_manager.helpers.coveralls_helper import CoverallsHelper
+
+
+class CoverallsHelperTest(unittest.TestCase):
+    def setUp(self):
+        self.coveralls = CoverallsHelper('jlmdegoede', 'MOOCworkbench')
+        self.non_existent_coveralls = CoverallsHelper('jlmdegoede', 'NON_EXISTENT_REPOSITORY')
+
+    def test_init(self):
+        self.assertIsNotNone(self.coveralls)
+
+    def test_code_coverage_data(self):
+        self.assertIsInstance(self.coveralls.code_coverage_data(), float)
+
+    def test_code_coverage_data_non_existent(self):
+        self.assertIsNone(self.non_existent_coveralls.code_coverage_data())
+
+    def test_coverage_enabled_check(self):
+        self.assertTrue(self.coveralls.coverage_enabled_check())
+
+    def test_coverage_enabled_check_not_enabled(self):
+        self.assertFalse(self.non_existent_coveralls.coverage_enabled_check())
+
+    def test_get_badge_url(self):
+        badge_url = self.coveralls.get_badge_url()
+        val = URLValidator()
+        val(badge_url)
+
+    def test_get_badge_url_invalid(self):
+        badge_url = self.non_existent_coveralls.get_badge_url()
+        val = URLValidator()
+        self.assertRaises(ValidationError, val, badge_url)

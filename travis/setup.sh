03bfd2aeb3e790911d33f27bed197f52fa588fde
Jochem <jochem@degoede.email>
2017-05-07 15:57:09 +0200

    Fix Travis CI error

--- travis/setup.sh
+++ travis/setup.sh
@@ -1,4 +1,4 @@
 echo 'ALLOWED_HOSTS = ["*"]
 SECRET_KEY = "8w9eruajfhsdjfl3h4uihdfwekrw4t2rgergsergsetg"
 DEBUG = True
-GITHUB_WEBHOOK_KEY = '23n582v3uvbopw5tnugws;4uno3pw5ufg3w5pgnufnf3i;nu''> MOOCworkbench/production_settings.py
+GITHUB_WEBHOOK_KEY = "23n582v3uvbopw5tnugws;4uno3pw5ufg3w5pgnufnf3i;nu" '> MOOCworkbench/production_settings.py

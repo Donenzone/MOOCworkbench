6f469d9a969920f4c694780d6563717a300f275b
Jochem de Goede <jochem@degoede.email>
2017-05-26 11:27:39 +0200

    Package for publication should be zipped

--- marketplace/utils.py
+++ marketplace/utils.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import subprocess
 
 from django.conf import settings
 
@@ -21,6 +22,8 @@ def internalpackage_publish_update(package):
     if os.path.exists(dst_dir):
         shutil.rmtree(dst_dir)
     shutil.copytree(git_helper.repo_dir, dst_dir)
+    zip_name = '{0}.zip'.format(package.git_repo.name)
+    subprocess.call(['./shell_scripts/publish_package.sh', PACKAGES_DIR, package.git_repo.name, zip_name])
 
 
 def internalpackage_rename(package, old_name):
@@ -29,15 +32,16 @@ def internalpackage_rename(package, old_name):
     git_helper = GitHelper(github_helper)
     git_helper.clone_or_pull_repository()
 
+    old_name = '{0}.zip'.format(old_name)
     # copy repository to packages
-    dst_dir = os.path.join(PACKAGES_DIR, package.git_repo.name)
     old_dst_dir = os.path.join(PACKAGES_DIR, old_name)
     if os.path.exists(old_dst_dir):
         shutil.rmtree(old_dst_dir)
-    shutil.copytree(git_helper.repo_dir, dst_dir)
+    internalpackage_publish_update(package)
 
 
 def internalpackage_remove(package):
-    dst_dir = os.path.join(PACKAGES_DIR, package.git_repo.name)
+    package_file_name = '{0}.zip'.format(package.git_repo.name)
+    dst_dir = os.path.join(PACKAGES_DIR, package_file_name)
     if os.path.exists(dst_dir):
         shutil.rmtree(dst_dir)

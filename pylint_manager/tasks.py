5bfab5a5265feb7f9f0f7fd6d32bb12584bf7da3
Jochem de Goede <jochem@degoede.email>
2017-05-30 11:41:22 +0200

    Incorrect import in Pylint Manager

--- pylint_manager/tasks.py
+++ pylint_manager/tasks.py
@@ -1,11 +1,9 @@
 from MOOCworkbench.celery import app
-from git_manager.helpers.helper import get_experiment_from_repo_name
-
-from .utils import run_pylint
+from git_manager.helpers.helper import get_exp_or_package_from_repo_name
 
 
 @app.task
 def task_run_pylint(repository_name):
-    experiment = get_experiment_from_repo_name(repository_name)
+    experiment = get_exp_or_package_from_repo_name(repository_name)
     language_helper = experiment.language_helper()
     language_helper.static_code_analysis()(experiment)
